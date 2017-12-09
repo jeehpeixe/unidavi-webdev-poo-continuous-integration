c1a888e984f096fc0c9398314e5d07807ac1a030
Jéssica Aparecida Peixe <peixinhah@msn.com>
2017-12-09 09:22:02 -0200

    Criar um commit com a classe de testes automatizadas compilando mas com todos os testes ignorados

---
+++ src/main/java/com/mycompany/unidavi/webdev/poo/continuous/integration/WordCount.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.unidavi.webdev.poo.continuous.integration;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author jessicapeixe
+ */
+public class WordCount {
+    
+    static private Map<String, Integer> lista = new TreeMap<>();
+    
+    public Map<String, Integer> phrase(String sValor) {
+        return lista;
+    }
+    
+}
